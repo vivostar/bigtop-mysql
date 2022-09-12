47412bef484a671151a717e80416b752571d4587
vivostar <lishuaipeng651@gmail.com>
2022-09-13 00:55:24 +0800

    today work good

--- manifests/cluster.pp
+++ manifests/cluster.pp
@@ -16,8 +16,15 @@ class node_with_roles($roles = hiera("bigtop::roles")) {
     "azkaban",  
   ]
 
-  node_with_roles::deploy_module { $modules:
-    roles => $roles,
+  $modules.each |$key, $value| {
+    notice($value)
+    class { "${value}::deploy":
+      roles => $roles,
+    }
   }
+
+  # Here is very useful to change the stage puppet actually execution.
+  # Todo, here's stage organization is not good, I will tune it tomorrow.
+  Mysql::Db<||> -> Archive<||>
   
 }
