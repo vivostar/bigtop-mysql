703b9c4309f4e6cd2eefcdbdb19ee6c274e243c1
vivostar <lishuaipeng651@gmail.com>
2022-09-13 00:59:06 +0800

    add some useful comments

--- manifests/cluster.pp
+++ manifests/cluster.pp
@@ -2,10 +2,6 @@
 class node_with_roles($roles = hiera("bigtop::roles")) {
   
   define deploy_module($roles) {
-    $title.each |$key, $value| {
-      notice($value)
-    }
-    notice($name)
     class { "${name}::deploy":
       roles => $roles,
     }
@@ -16,13 +12,18 @@ class node_with_roles($roles = hiera("bigtop::roles")) {
     "azkaban",  
   ]
 
-  $modules.each |$key, $value| {
-    notice($value)
-    class { "${value}::deploy":
-      roles => $roles,
-    }
+  # $modules.each |$key, $value| {
+  #   notice($value)
+  #   class { "${value}::deploy":
+  #     roles => $roles,
+  #   }
+  # }
+  # above equal following
+  node_with_roles::deploy_module { $modules:
+    roles => $roles,
   }
 
+  # relate to Puppet Resource Collectors and Relationships and ordering
   # Here is very useful to change the stage puppet actually execution.
   # Todo, here's stage organization is not good, I will tune it tomorrow.
   Mysql::Db<||> -> Archive<||>
