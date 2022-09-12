4ad0642e4c3d256ac9ad89ea83f1ce5927eef042
vivostar <lishuaipeng651@gmail.com>
2022-09-13 00:08:17 +0800

    today work

--- manifests/cluster.pp
+++ manifests/cluster.pp
@@ -2,17 +2,22 @@
 class node_with_roles($roles = hiera("bigtop::roles")) {
   
   define deploy_module($roles) {
+    $title.each |$key, $value| {
+      notice($value)
+    }
+    notice($name)
     class { "${name}::deploy":
-    roles => $roles,
+      roles => $roles,
     }
   }
 
   $modules = [
-    "azkaban",
     "mysql_deploy",
+    "azkaban",  
   ]
 
   node_with_roles::deploy_module { $modules:
     roles => $roles,
   }
+  
 }
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,24 +1,5 @@
 
 
-# node default {
+node default {
   include node_with_roles
-# }
-
-class node_with_roles($roles = hiera("bigtop::roles")) {
-  
-  define deploy_module($roles) {
-    class { "${name}::deploy":
-    roles => $roles,
-    }
-  }
-
-  $modules = [
-    "azkaban",
-    "mysql_deploy",
-  ]
-
-  node_with_roles::deploy_module { $modules:
-    roles => $roles,
-  }
 }
-
